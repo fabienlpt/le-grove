e5ab5b2e871aeeecaffdec4396c88f36919ecb59
Fabien Lapert <lapertfabien@gmail.com>
2023-11-15 12:52:27 +0100

    add variable

---
+++ src/services/theme/index.ts
@@ -0,0 +1,26 @@
+import { Theme } from "../../types/theme";
+
+const theme: Theme = {
+	colors: {
+		layout: {
+			body: "#F9F2DE",
+			primary: "#144D3A",
+			secondary: "#C5FACC",
+		},
+		text: {
+			primary: "#C5FACC",
+			secondary: "#144D3A",
+			tertiary: "#F9F2DE ",
+		},
+	},
+	fontSize: {
+		tiny: "11px",
+		small: "13px",
+		normal: "16px",
+		medium: "20px",
+		large: "24px",
+		title: "32px",
+	},
+};
+
+export default theme;
