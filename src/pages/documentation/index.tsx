0b855ba013fd8a98bf0680ee2e5b82f15b64746d
Fabien Lapert <lapertfabien@gmail.com>
2023-11-16 10:53:30 +0100

    update doc page

--- src/pages/documentation/index.tsx
+++ src/pages/documentation/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import SideBar from '../components/sidebar';
+import SideBar from '../../components/sidebar';
+import './style.css';
 
 const Documentation: React.FC = () => {
     return (
-        <div className='page-container'>
+        <div className='page-container doc-page'>
             {/* TODO: PRIO 1 */}
             {/* documentation page */}
             <SideBar />
