0e6e9697fb94e558f1e361f3dc4ae9ebbae41afa
Fabien Lapert <lapertfabien@gmail.com>
2023-11-16 11:38:00 +0100

    update navbutton

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import NavButton from '../header/navButton';
+import NavButton from '../navButton';
 
 const Footer: React.FC = () => {
     const [current, setCurrent] = React.useState<string>(window.location.pathname);
@@ -48,9 +48,9 @@ const Footer: React.FC = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" height="2em" viewBox="0 0 448 512"><style></style><path d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"/></svg>
                     </div>
                     <div className="onglets">
-                        <NavButton to="/missions" label="nos missions" current={current} setCurrent={setCurrent} className="missions" />
-                        <NavButton to="/doc" label="nos ressources" current={current} setCurrent={setCurrent} className="doc" />
-                        <NavButton to="/contact" label="contact" current={current} setCurrent={setCurrent} className="contact" />
+                        <NavButton to="/missions" label="nos missions" current={current} setCurrent={setCurrent} withPin={false} className="missions" />
+                        <NavButton to="/doc" label="nos ressources" current={current} setCurrent={setCurrent} withPin={false} className="doc" />
+                        <NavButton to="/contact" label="contact" current={current} setCurrent={setCurrent} withPin={false} className="contact" />
                     </div>
                 </div>
             </div>
